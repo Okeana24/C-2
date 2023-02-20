1137aedf44cca9a5199d1522b64fe27f960211da
Olga <vvs.olga@yandex.ru>
2023-02-20 19:39:46 +0400

    first commit

--- zadacha3.py
+++ zadacha3.py
@@ -1,13 +1,19 @@
-var x, y, S, P: integer;
-
-begin
-
-   readln(S, P);
-
-   for x := 1 to 1000 do
-
-       for y := 1 to 1000 do
-
-           if (x + y = S) and (x * y = P) then
-
-               writeln(x, ' ', y);
+x = abs(int(input('Введите первое натуральное число X от 1 до 1000 ')))
+y = abs(int(input('Введите второе натуральное число Y от 1 до 1000 ')))
+if x < 1 or x > 1000 or y < 1 or y > 1000:
+    print('Вы ввели число не из заданного диапазона!')
+else:
+    S = x + y
+    P = x * y
+    stop = 0
+    for i in range(1001):
+        if stop != 1:
+            for j in range(1001):
+                if stop != 1:
+                    if i * j == P and i + j == S:
+                        print(i, j)
+                        stop = 1
+            else:
+                j = 1001
+        else:
+            i = 1001
--- zadacha4.py
+++ zadacha4.py
@@ -1,23 +1,14 @@
-package main
-
-import (
-	"fmt"
-	"math"
-)
-
-func main() 
-	var n int
-	var outputStr string
-
-	_, _ = fmt.Scan(&n)
-
-	for i := 0; i <= n; i++ 
-		x := int(math.Exp2(float64(i)))
-		if x > n {
-			break
-		
-		outputStr += fmt.Sprintf("%d ", x)
-	
-
-	fmt.Println(outputStr)
+N = abs(int(input('Введите число N ')))
+stop = 0
+P = 2
+for i in range(N):
+    if stop != 1:
+        P = P ** i
+        if P <= N:
+            print(P, end=' ')
+            P = 2
+        else:
+            stop = 1
+    else:
+        i = N
 
